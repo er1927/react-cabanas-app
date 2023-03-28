ad23a46f24fd6f3c539fb7d1bbced7e214f559a0
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-03-28 18:01:13 -0300

    Fixed overflowing text from img

--- src/Cabins.js
+++ src/Cabins.js
@@ -4,13 +4,13 @@ export default function Cabins() {
     return(
         <div class="main" id="cabins">
         <div class="six">
-          <a href=""><h2>Cabaña para 6 personas</h2></a>
+          <h2>Cabaña para 6 personas</h2>
         </div>
         <div class="five">
-          <a href=""><h2>Cabaña para 5 personas</h2></a>
+          <h2>Cabaña para 5 personas</h2>
         </div>
         <div class="four">
-          <a href=""><h2>Cabaña para 4 personas</h2></a>
+          <h2>Cabaña para 4 personas</h2>
         </div>
       </div>        
     )
