de34bd810ab270ff0acffb377853567acab700f5
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-03-29 14:22:27 -0300

    Added carousel for 6pax cabin

--- src/Cabins.js
+++ src/Cabins.js
@@ -1,10 +1,12 @@
 import React from "react"
+import MyGallery from "./Six"
 
 export default function Cabins() {
     return(
         <div class="main" id="cabins">
         <div class="six">
           <h2>Cabaña para 6 personas</h2>
+          <MyGallery />
         </div>
         <div class="five">
           <h2>Cabaña para 5 personas</h2>
